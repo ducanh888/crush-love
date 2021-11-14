d1803fa7b1165bd2423269df963b52e3633cef50
ducanh888 <94307778+ducanh888@users.noreply.github.com>
2021-11-14 23:17:35 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Anh biết là em sẽ đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được che chở và bảo vệ em.Đối với anh, em mãi là cô bé :)) </br> "Thích em nhiều đến mấy, em sẽ hiểu thôi. "',
 };
